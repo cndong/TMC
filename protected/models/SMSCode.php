f669d831aeb4890640cad1f16cb73b0ab237b57d
suiyongjie <suiyongjie@meiti.com>
2016-05-27 13:24:06 +0800

    modify verify

--- protected/models/SMSCode.php
+++ protected/models/SMSCode.php
@@ -87,6 +87,6 @@ class SMSCode extends QActiveRecord {
             return F::errReturn(RC::RC_MODEL_UPDATE_ERROR);
         }
         
-        return F::corReturn();
+        return $this->status == self::STATUS_VERIFY_SUCCEED ? F::corReturn() : F::errReturn(RC::RC_SMS_CODE_NOT_CORRECT);
     }
 }
