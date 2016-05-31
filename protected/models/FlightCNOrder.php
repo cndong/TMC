c2f3ae479bac9f880da81d1d8d1e11408ac5affd
suiyongjie <suiyongjie@meiti.com>
2016-05-31 14:44:31 +0800

    modify cabinnum

--- protected/models/FlightCNOrder.php
+++ protected/models/FlightCNOrder.php
@@ -231,7 +231,7 @@ class FlightCNOrder extends QActiveRecord {
                     return F::errReturn(RC::RC_F_INFO_CHANGED);
                 }
     
-                if ((!is_numeric($realCabin['cabinNum']) && $realCabin['cabinNum'] != 'A') || (intval($realCabin['cabinNum']) < count($params['passengers']))) {
+                if ((!is_numeric($realCabin['cabinNum']) && $realCabin['cabinNum'] != 'A') || (is_numeric($realCabin['cabinNum']) && intval($realCabin['cabinNum']) < count($params['passengers']))) {
                     return F::errReturn(RC::RC_F_CABIN_NUM_ERROR);
                 }
     
