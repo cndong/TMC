d9920479a56b57f1ead299b6df87ca1d270bccc4
suiyongjie <suiyongjie@meiti.com>
2016-06-07 11:02:41 +0800

    modify company finance log

--- protected/models/Company.php
+++ protected/models/Company.php
@@ -102,7 +102,7 @@ class Company extends QActiveRecord {
         }
         
         $this->finance += $num;
-        if (!F::isCorrect($res = CompanyFinance::create($this, $type, $payout, $income, $info))) {
+        if (!F::isCorrect($res = CompanyFinanceLog::create($this, $type, $payout, $income, $info))) {
             return $res;
         }
         
