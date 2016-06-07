3c28932133051f33eb232a25f05a9b8b32b59601
suiyongjie <suiyongjie@meiti.com>
2016-06-07 11:09:25 +0800

    modify company finance log template

--- protected/models/CompanyFinanceLog.php
+++ protected/models/CompanyFinanceLog.php
@@ -8,11 +8,11 @@ class CompanyFinanceLog extends QActiveRecord {
     const TYPE_REFUND = 6;
     public static $types = array(
         self::TYPE_RECHARGE => array('name' => '充值'),
-        self::TYPE_ORDER_PRICE => array('name' => '票价(含税)', 'template' => '订单号:<{orderID}>，部门:<{departmentName}>，用户:<{$userName}>'),
-        self::TYPE_INSURE_PRICE => array('name' => '保险', 'template' => '订单号:<{orderID}>，部门:<{departmentName}>，用户:<{$userName}>'),
-        self::TYPE_INVOICE_PRICE => array('name' => '邮费', 'template' => '订单号:<{orderID}>，部门:<{departmentName}>，用户:<{$userName}>'),
-        self::TYPE_RESIGN_PRICE => array('name' => '改签', 'template' => '订单号:<{orderID}>，部门:<{departmentName}>，用户:<{$userName}>'),
-        self::TYPE_REFUND => array('name' => '退票', 'template' => '订单号:<{orderID}>，部门:<{departmentName}>，用户:<{$userName}>')
+        self::TYPE_ORDER_PRICE => array('name' => '票价(含税)', 'template' => '订单号:<{orderID}>，部门:<{departmentName}>，用户:<{userName}>'),
+        self::TYPE_INSURE_PRICE => array('name' => '保险', 'template' => '订单号:<{orderID}>，部门:<{departmentName}>，用户:<{userName}>'),
+        self::TYPE_INVOICE_PRICE => array('name' => '邮费', 'template' => '订单号:<{orderID}>，部门:<{departmentName}>，用户:<{userName}>'),
+        self::TYPE_RESIGN_PRICE => array('name' => '改签', 'template' => '订单号:<{orderID}>，部门:<{departmentName}>，用户:<{userName}>'),
+        self::TYPE_REFUND => array('name' => '退票', 'template' => '订单号:<{orderID}>，部门:<{departmentName}>，用户:<{userName}>')
     );
     
     public static function model($className = __CLASS__) {
@@ -26,7 +26,7 @@ class CompanyFinanceLog extends QActiveRecord {
         return array(
             array('companyID, type, income, payout, finance, info', 'required'),
             array('companyID, type, income, payout, finance, ctime, utime', 'numerical', 'integerOnly'=>true),
-            array('info', 'length', 'max' => 255),
+            array('info', 'length', 'max' => 500),
             array('id, companyID, type, income, payout, finance, ctime, utime', 'safe', 'on' => 'search'),
         );
     }
