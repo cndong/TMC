c9f04941d360245c00ae31e387425ae98bb755c1
root <root@AY140321180108749ab0Z.(none)>
2016-06-16 18:27:18 +0800

    modify flightOrderList.js

--- static/js/pc/boss/flightOrderList.js
+++ static/js/pc/boss/flightOrderList.js
@@ -158,7 +158,7 @@ $(function() {
 				if ($(this).attr("data-flag") == "beginTime") {
 					WdatePicker({dateFmt:'yyyy-MM-dd HH:mm', minDate: '%y-%M-%d {%H-3}:%m'});
 				} else {
-					WdatePicker({dateFmt:'yyyy-MM-dd HH:mm', minDate: '#F{$(".c_time[data-flag=\'endTime\']").val()}'});
+					WdatePicker({dateFmt:'yyyy-MM-dd HH:mm', minDate: '#F{$(\'.c_time\').eq(0).val()}'});
 				}
 					
 			});
