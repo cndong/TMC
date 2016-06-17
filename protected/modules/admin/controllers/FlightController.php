d68cb3db8e710452fa77d47e424e5e856ea9d9f2
suiyongjie <suiyongjie@meiti.com>
2016-06-17 13:23:46 +0800

    modify admin show

--- protected/modules/admin/controllers/FlightController.php
+++ protected/modules/admin/controllers/FlightController.php
@@ -1,18 +1,9 @@
 <?php
 class FlightController extends AdminController {
     public function actionOrderList() {
-        $searchTypes = array(
-            'orderID' => '订单ID',
-        );
-        
         $_GET['companyID'] = $this->admin->company->id;
         $_GET['isPrivate'] = Dict::STATUS_FALSE;
-        $searchParams = $_GET;
-        $searchParams['searchType'] = !empty($searchParams['searchType']) && isset($searchTypes[$searchParams['searchType']]) ? $searchParams['searchType'] : False;
-        if ($searchParams['searchType']) {
-            $searchParams[$searchParams['searchType']] = empty($searchParams['searchValue']) ? '' : $searchParams['searchValue'];
-        }
-        $data = FlightCNOrder::search($searchParams, True);
+        $data = FlightCNOrder::search($_GET, True);
         $dataProvider = new CActiveDataProvider('FlightCNOrder', array(
             'criteria' => $data['criteria'],
             'pagination' => array(
@@ -21,6 +12,6 @@ class FlightController extends AdminController {
         ));
         
         $this->setRenderParams('breadCrumbs', array('飞机票', '订单列表'));
-        $this->render('orderList', array('dataProvider' => $dataProvider, 'params' => $data['params'], 'searchTypes' => $searchTypes));
+        $this->render('orderList', array('dataProvider' => $dataProvider, 'params' => $data['params']));
     }
 }
--- protected/modules/admin/views/flight/orderList.php
+++ protected/modules/admin/views/flight/orderList.php
@@ -7,21 +7,7 @@
             <div class="panel-heading">
                 <form class="form-inline" name="searchForm">
                     <span>订单列表</span>
-                    <select name="searchType" class="form-control input-sm" data-default-value="0">
-                        <option value="0">--搜索类别--
-                        <?php 
-                        $searchValue = '';
-                        foreach ($searchTypes as $searchType => $searchName) {
-                            $selected = '';
-                            if (!empty($params[$searchType])) {
-                                $selected = ' selected';
-                                $searchValue = $params[$searchType];
-                            }
-                            echo "<option value='{$searchType}'{$selected}>{$searchName}";
-                        }
-                        ?>
-                    </select>
-                    <input name="searchValue" type="text" class="form-control input-sm" size="6" value="<?php echo $searchValue; ?>" data-default-value="" />
+                    <input name="orderID" type="text" class="form-control input-sm" size="6" value="<?php echo $params['orderID']; ?>" data-default-value="" />
                     <label>起始时间</label>
                     <input name="beginDate" type="text" data-flag="beginDate" class="c_search_time form-control input-sm" size="10" value="<?php echo $params['beginDate']; ?>" data-default-value="" />
                     <label>结束时间</label>
