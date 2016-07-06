abb6a15a9634fc6b678af4b6ad4f6266ae4a7164
cndong.cn <cndong.cn@gmail.com>
2016-07-06 17:50:54 +0800

    adjust debug for train curl

--- protected/extensions/Curl.php
+++ protected/extensions/Curl.php
@@ -60,7 +60,7 @@ class Curl {
 	}
 	
 	public function debug() {
-	    $this->initHandler(array(CURLOPT_PROXY => '127.0.0.1:8888'));
+	    Q::isLocalEnv() && $this->initHandler(array(CURLOPT_PROXY => '127.0.0.1:8888'));
 	    return $this;
 	}
 	
