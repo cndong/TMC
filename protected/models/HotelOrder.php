4151edd6416f1815ef925ca7b486c515bfbc4ef5
cndong.cn <cndong.cn@gmail.com>
2016-07-06 17:48:39 +0800

    add isPrivate for hotelOrder

--- protected/models/HotelOrder.php
+++ protected/models/HotelOrder.php
@@ -12,7 +12,7 @@ class HotelOrder extends QActiveRecord {
     
     public function rules() {
         return array(
-            array('merchantID, userID, departmentID, companyID, hotelId, roomId, rateplanId, ctime, utime, status,roomCount, orderPrice, bedLimit, breakfast, reviewerID', 'numerical'),
+            array('merchantID, userID, departmentID, companyID, hotelId, roomId, rateplanId, ctime, utime, status,roomCount, orderPrice, bedLimit, breakfast, reviewerID, isPrivate', 'numerical'),
             array('hotelName, bookName, guestName, oID, roomName', 'length', 'max' => 32),
             array('reason, specialRemark', 'length', 'max' => 64),
             array('checkIn, checkOut', 'length', 'max' => 10),
