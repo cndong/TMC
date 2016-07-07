96fa21c4e22cd3743920c5a81b56ad93e4de8e88
suiyongjie <suiyongjie@meiti.com>
2016-07-07 16:26:27 +0800

    modify book push test

--- protected/modules/api/controllers/TestController.php
+++ protected/modules/api/controllers/TestController.php
@@ -2,6 +2,6 @@
 class TestController extends QController {
     public function actionBookPush($orderID) {
         $order = TrainOrder::model()->findByPk($orderID);
-        var_dump($order->changeStatus(TrainStatus::BOOK_PUSHED));
+        var_dump($order->changeStatus(TrainStatus::BOOK_PUSHED, array('operaterID' => Dict::OPERATER_SYSTEM)));
     }
 }
