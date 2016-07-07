1bc0574c20ed3be99699556b44bf92cab990c053
suiyongjie <suiyongjie@meiti.com>
2016-07-07 18:23:05 +0800

    modify test

--- protected/modules/api/controllers/ProviderController.php
+++ protected/modules/api/controllers/ProviderController.php
@@ -14,28 +14,6 @@ class ProviderController extends ApiController {
     }
     
     public function actionTrainMergeRes() {
-        /*
-        if (Q::isLocalEnv()) {
-            $_POST = array (
-              'status' => '1',
-              'trainNo' => '6021',
-              'arriveStation' => '周家',
-              'departStationCode' => 'niujia',
-              'sign' => 'dc736ba24338ec396a36504f53f6a1f4',
-              'merchantOrderID' => '6',
-              'arriveStationCode' => 'zhoujia',
-              'departDateTime' => '2016-08-22 12:34',
-              'orderID' => 'TAG4D0211900015',
-              'passengers' => '[{"type":"1","name":"\\u5355\\u9752\\u82b3","cardType":"1","cardNo":"330124196908282125","seatType":"2","id":"TAG4F0291910018","seatName":"14\\u8f66\\u53a2,06F\\u5ea7","price":"1.0"}]',
-              'requestTime' => '1467872323',
-              'pickNo' => 'E622851719',
-              'departStation' => '牛家',
-              'servicePrice' => '1.5',
-              'merchantID' => '12',
-            );
-        }
-        */
-        
         if (!F::checkParams($_POST, array('merchantOrderID' => ParamsFormat::INTNZ, 'status' => ParamsFormat::BOOL, 'passengers' => ParamsFormat::JSON))) {
             $this->_doTrainFail(RC::RC_VAR_ERROR);
         }
--- protected/modules/api/controllers/TestController.php
+++ protected/modules/api/controllers/TestController.php
@@ -1,5 +1,11 @@
 <?php
 class TestController extends QController {
+    public function init() {
+        if (!Q::isLocalEnv() && !Q::isTestEnv()) {
+            Yii::app()->end('Bye Bye');
+        }
+    }
+    
     public function actionBookPush($orderID) {
         $order = TrainOrder::model()->findByPk($orderID);
         var_dump($order->changeStatus(TrainStatus::BOOK_PUSHED, array('operaterID' => Dict::OPERATER_SYSTEM)));
